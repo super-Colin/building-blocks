7b76b56603c140ccc9d3ba40330a9f991fa428cb
superColin <supercolin.dev@gmail.com>
2020-02-29 19:08:05 -0500

    made horizontal scroll progress bar

---
+++ image-ripper/image-ripper.js
@@ -0,0 +1,5 @@
+
+const ripper = document.getElementById('ripper-inner');
+
+
+
---
+++ scroll-progress/scroll-progress.js
@@ -0,0 +1,17 @@
+
+const scrollProgressBar = document.getElementById('scroll-progress-bar');
+window.onscroll = scrollBarUpdate;
+
+function scrollBarUpdate(){
+    const spot = window.pageYOffset;
+    const height = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight) - window.innerHeight;
+    const percentScrolled = ((spot / height) * 100);
+    scrollProgressBar.style.width = (percentScrolled + "%")
+}
+
+
+// None of these worked
+// window.addEventListener('scroll', scrollBarUpdate(), {passive: true});
+// document.body.addEventListener('scroll', scrollBarUpdate(), {passive: true});
+// document.documentElement.addEventListener('scroll', scrollBarUpdate(), {passive: true});
+// None of these worked
